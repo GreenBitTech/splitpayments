c4dcb4377cccf2108bcd39192a6f90cad5613c5d
Brett Jones <42926044+GreenBitTech@users.noreply.github.com>
2025-05-11 00:00:17 +0800

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -47,7 +47,7 @@ async def on_invoice_paid(payment: Payment) -> None:
             amount_msat = int(payment.amount * target.percent / 100)
             memo = (
                 f"GreenBit mining reward for "
-                f"{target.alias or target.wallet};"
+                f"{target.alias or target.wallet}"
             )
 
             if "@" in target.wallet or "LNURL" in target.wallet:
